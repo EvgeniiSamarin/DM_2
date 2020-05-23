7e476660da2ae438b567cbba59db93547488a908
Eugene Samari <sam.201498@gmail.com>
2020-05-23 11:12:59 +0400

    .

--- src/main/java/ru/kpfu/itis/Samarin903/Bucket.java
+++ src/main/java/ru/kpfu/itis/Samarin903/Bucket.java
@@ -22,10 +22,16 @@ public class Bucket {
         String csvFilename = "src/transactions.csv";
         CSVReader csvReader = new CSVReader(new FileReader(csvFilename), ';', '"', 1);
         List list = csv.parse(setColumnMapping(), csvReader);
-        Map<String, Map<Integer, List<String>>> products = new Bucket().addAllProducts(list); // [ PRDxxxxxxx [ number_of_orders list_of_baskets ] ]
+        Map<String, Map<Integer, List<String>>> products = new Bucket().addAllProducts(list);
         writeToCSV(products);
     }
-
+    private static ColumnPositionMappingStrategy<Bucket> setColumnMapping() {
+        ColumnPositionMappingStrategy<Bucket> strategy = new ColumnPositionMappingStrategy<Bucket>();
+        strategy.setType(Bucket.class);
+        String[] columns = new String[] {"prodCode", "basketID"};
+        strategy.setColumnMapping(columns);
+        return strategy;
+    }
     private static void writeToCSV(Map<String, Map<Integer, List<String>>> products) throws IOException {
         String csv = "target/data.csv";
         CSVWriter writer = new CSVWriter(new FileWriter(csv));
@@ -40,13 +46,7 @@ public class Bucket {
         writer.close();
     }
 
-    private static ColumnPositionMappingStrategy<Bucket> setColumnMapping() {
-        ColumnPositionMappingStrategy<Bucket> strategy = new ColumnPositionMappingStrategy<Bucket>();
-        strategy.setType(Bucket.class);
-        String[] columns = new String[] {"prodCode", "basketID"};
-        strategy.setColumnMapping(columns);
-        return strategy;
-    }
+
 
     private Map<String, Map<Integer, List<String>>> addAllProducts(List list) {
         Map<String, Map<Integer, List<String>>> products = new HashMap<String, Map<Integer, List<String>>>();
